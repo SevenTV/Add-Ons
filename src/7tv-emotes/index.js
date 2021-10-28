95ba2b56167fee6aec77256965ece66fdcb69f09
Melonify <13710061+Melonify@users.noreply.github.com>
2021-10-27 21:46:54 -0400

    Partial animated avatar implementation

--- src/7tv-emotes/index.js
+++ src/7tv-emotes/index.js
@@ -7,6 +7,7 @@ class SevenTVEmotes extends Addon {
 		this.inject('chat.emotes');
 		this.inject('chat.badges');
 		this.inject('site');
+		this.inject('site.fine');
 		this.injectAs('siteChat', 'site.chat');
 
 		this.settings.add('addon.seventv_emotes.global_emotes', {
@@ -69,7 +70,19 @@ class SevenTVEmotes extends Addon {
 			}
 		});
 
+		this.settings.add('addon.seventv_emotes.animated_avatars', {
+			default: true,
+			ui: {
+				path: 'Add-Ons > 7TV Emotes >> User Cosmetics',
+				title: 'Animated Avatars',
+				description: 'Show 7TV animated avatars. [(7TV Subscriber Perk)](https://7tv.app/subscribe)',
+				component: 'setting-check-box',
+			}
+		});
+
 		this.bulkBadgeIDs = new Set();
+
+		this.userAvatars = new Map();
 	}
 
 	async onEnable() {
@@ -81,6 +94,7 @@ class SevenTVEmotes extends Addon {
 		});
 		this.chat.context.on('changed:addon.seventv_emotes.unlisted_emotes', () => this.updateChannelSets());
 		this.chat.context.on('changed:addon.seventv_emotes.emote_updates', () => this.updateEventSource());
+		this.chat.context.on('changed:addon.seventv_emotes.animated_avatars', () => this.updateAnimatedAvatars());
 
 		this.on('chat:room-add', this.addChannel, this);
 		this.on('chat:room-remove', this.removeChannel, this);
@@ -89,6 +103,11 @@ class SevenTVEmotes extends Addon {
 		this.updateGlobalEmotes();
 		this.updateChannelSets();
 		this.updateEventSource();
+		this.updateAnimatedAvatars();
+
+		this.patchAvatarRenderer();
+
+		window.sevenTV = this;
 	}
 
 	addChannel(channel) {
@@ -384,6 +403,61 @@ class SevenTVEmotes extends Addon {
 				this.siteChat.addNotice(channel.login, message);
 			}
 		}
+	}
+
+	async patchAvatarRenderer() {
+		if (this.root.flavor != "main") return;
+
+		let avatarElement = await this.site.awaitElement(".tw-avatar");
+
+		if (avatarElement) {
+			let avatarComponent = this.site.fine.getOwner(avatarElement);
+
+			if (avatarComponent.type.displayName == "ScAvatar") {
+				let oldRenderer = avatarComponent.type.render;
+
+				avatarComponent.type.render = (component, ...args) => {
+					for (let child of component.children) {
+						if (child?.type?.displayName == "ImageAvatar") this.patchImageAvatar(child);
+					}
+					return oldRenderer(component, ...args);
+				}
+			}
+		}
+	}
+
+	patchImageAvatar(component) {
+		let props = component.props;
+		if (props.userLogin && props["data-a-target"] != "profile-image") {
+			let animatedAvatarURL = this.getUserAvatar(props.userLogin);
+			if (animatedAvatarURL) {
+				props.SEVENTV_oldSrc = props.SEVENTV_oldSrc || props.src;
+				props.src = animatedAvatarURL;
+			}
+			else if (props.SEVENTV_oldSrc) {
+				props.src = props.SEVENTV_oldSrc;
+				delete props["SEVENTV_oldSrc"];
+			}
+		}
+	}
+
+	async updateAnimatedAvatars() {
+		this.userAvatars.clear();
+
+		if (!this.chat.context.get('addon.seventv_emotes.animated_avatars')) return;
+
+		const response = await fetch("https://api.7tv.app/v2/cosmetics/avatars?map_to=login");
+		if (response.ok) {
+			const json = await response.json();
+
+			for (const [login, avatar] of Object.entries(json)) {
+				this.userAvatars.set(login, avatar);
+			}
+		}
+	};
+
+	getUserAvatar(login) {
+		return this.userAvatars.get(login.toLowerCase());
 	}
 }
 
